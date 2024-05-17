15cd18d253c4147ed1c46987195cb61902d84b68
Mooner510 <lmck510@gmail.com>
2024-05-17 14:25:02 +0900

    abrakatabura ni av okk opiano konowizano kyunikai ninianokushitau

---
+++ src/main/kotlin/kr/mooner510/konopuro/domain/game/data/card/dto/GameStudentCard.kt
@@ -0,0 +1,93 @@
+package kr.mooner510.konopuro.domain.game.data.card.dto
+
+import kr.mooner510.konopuro.domain.game._preset.PassiveType
+import kr.mooner510.konopuro.domain.game._preset.StudentCardType
+import kr.mooner510.konopuro.domain.game._preset.TierType
+import kr.mooner510.konopuro.domain.game.data.card.types.StudentState
+import kr.mooner510.konopuro.domain.game.data.global.types.MajorType
+import org.json.JSONObject
+import java.util.*
+
+data class GameStudentCard(
+    val id: UUID,
+    val cardType: StudentCardType,
+    val groups: Set<MajorType>,
+    val tiers: EnumSet<TierType>,
+    val passives: EnumSet<PassiveType>
+) {
+    private var fatigue: Double = 0.0
+
+    private val dataInt = EnumMap<StudentState, Int>(StudentState::class.java)
+
+    enum class Modifier {
+        Fatigue,
+        Int
+    }
+
+    fun getFatigue() = fatigue
+
+    fun getEndDate(key: StudentState): Int? {
+        return dataInt[key]
+    }
+
+    fun getEndDate(key: StudentState, default: Int): Int {
+        return dataInt.getOrDefault(key, default)
+    }
+
+    fun checkEndDate(key: StudentState, now: Int): Boolean {
+        return now <= getEndDate(key, 0)
+    }
+
+    fun hasEndDate(key: StudentState): Boolean {
+        return dataInt.containsKey(key)
+    }
+
+    class GameStudentCardModifier(
+        private val date: Int,
+        private val gameStudentCard: GameStudentCard
+    ) {
+        private val modifier = EnumSet.noneOf(Modifier::class.java)
+
+        fun setFatigue(value: Double) {
+            gameStudentCard.fatigue = value
+            modifier.add(Modifier.Fatigue)
+        }
+
+        fun addFatigue(value: Double) {
+            gameStudentCard.fatigue += value
+            modifier.add(Modifier.Fatigue)
+        }
+
+        fun removeIfEndDate(now: Int) {
+            val iterator = gameStudentCard.dataInt.iterator()
+            iterator.forEachRemaining {
+                if (it.value < now) {
+                    iterator.remove()
+                    modifier.add(Modifier.Int)
+                }
+            }
+        }
+
+        fun setEndDate(key: StudentState, value: Int) {
+            gameStudentCard.dataInt[key] = date + value
+            modifier.add(Modifier.Int)
+        }
+
+        fun addEndDate(key: StudentState, value: Int) {
+            gameStudentCard.dataInt.merge(key, value, Integer::sum)
+            modifier.add(Modifier.Int)
+        }
+
+        fun build(): String? {
+            if (modifier.isEmpty()) return null
+            val json = JSONObject().put("id", gameStudentCard.id.toString())
+            if (modifier.contains(Modifier.Fatigue)) json.put("fatigue", gameStudentCard.getFatigue())
+            if (modifier.contains(Modifier.Int)) {
+                val data = JSONObject()
+                gameStudentCard.dataInt.forEach { (key, value) -> data.put(key.toString(), value) }
+                json.put("data", data)
+            }
+            return json.toString()
+        }
+    }
+}
