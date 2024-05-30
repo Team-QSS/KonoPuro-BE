0c854b3855986758bcd81a7b51976c00722cc5a6
hsung3407 <azxccxzavv3407@gmail.com>
2024-05-30 17:05:53 +0900

    Update Protocol.kt

--- src/main/kotlin/kr/mooner510/konopuro/domain/socket/data/Protocol.kt
+++ src/main/kotlin/kr/mooner510/konopuro/domain/socket/data/Protocol.kt
@@ -38,31 +38,37 @@ object Protocol {
         }
 
         object Server {
-            const val GAME_END = 200
-            const val SUCCESS_CARD = 201
-            const val SUCCESS_ABILITY = 202
-            const val NEW_DAY = 203
-            const val SLEEP = 204
+            const val GAME_START = 200
+            const val GAME_END = 201
+            const val SUCCESS_CARD = 202
+            const val SUCCESS_ABILITY = 203
+            const val NEW_DAY = 204
+            const val SLEEP = 205
 
             /**
              * 덱에서 서버가 카드 추가
              */
-            const val NEW_CARD = 205
+            const val NEW_CARD = 206
 
             /**
              * 패 업데이트
              */
-            const val HELD_UPDATE = 206
+            const val HELD_UPDATE = 207
 
             /**
              * 필드 업데이트
              */
-            const val FIELD_UPDATE = 207
+            const val FIELD_UPDATE = 208
 
             /**
              * 프로젝트 진행도 업데이트
              */
-            const val PROJECT_UPDATE = 208
+            const val PROJECT_UPDATE = 209
+
+            /**
+             * 플레이어 턴 설정
+             */
+            const val SET_TURN = 210
         }
     }
 }
